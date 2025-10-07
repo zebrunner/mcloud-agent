d61f9a4e2714f445c54b306351058ce6f3b4c58b
azarouski <azarouski@solvd.com>
2025-10-07 13:12:24 +0200

    sed delimiter was edited one more time

--- zebrunner.sh
+++ zebrunner.sh
@@ -141,7 +141,7 @@ shutdown() {
   TARGET_FILES+=("$HOME/.bashrc" "$HOME/.bash_profile" "$HOME/.zshrc" "$HOME/.zprofile" "$HOME/.profile")
   for file in "${TARGET_FILES[@]}"; do
     if [ -f "$file" ] && grep -q "export $MCLOUD_AGENT_DIR_NAME=" "$file"; then
-      sed -i.bak "|^export $MCLOUD_AGENT_DIR_NAME=.*|d" "$file" && rm -f "$file.bak"
+      sed -i.bak "/^export $MCLOUD_AGENT_DIR_NAME=.*/d" "$file" && rm -f "$file.bak"
       echo "Removed var $MCLOUD_AGENT_DIR_NAME in $file"
     fi
   done
