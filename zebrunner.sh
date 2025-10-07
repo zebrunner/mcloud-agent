9162ff20bcf338adc3d42a0f4941d22ae75df56c
azarouski <azarouski@solvd.com>
2025-10-07 21:02:05 +0200

    `./zebrunner.sh setup` process output was prettified

--- zebrunner.sh
+++ zebrunner.sh
@@ -121,6 +121,7 @@ setup() {
   echo ""
   #TODO: switch to master branch after official release and merge
   echo "Follow https://github.com/zebrunner/mcloud-agent/tree/master#run-ansible-playbook to deploy MCloud agent services!"
+  echo "==== Setting up MCloud agent in $BASEDIR finished successfully ===="
 }
 
 shutdown() {
