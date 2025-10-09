25c58ffc8af24cf8c1087cf50b748e57abdbe867
azarouski <azarouski@solvd.com>
2025-10-09 22:17:41 +0200

    `shutdown` function output was prettified

--- zebrunner.sh
+++ zebrunner.sh
@@ -288,7 +288,7 @@ shutdown() {
   if command -v zebrunner-farm >/dev/null 2>&1; then
     zebrunner-farm down
   else
-    echo "* Can't find 'zebrunner-farm' executable file"
+    echo "Can't find 'zebrunner-farm' executable file"
   fi
 
   echo -e "\n*******************************************************************\n"
