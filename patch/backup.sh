c90173d27f3aa0f4f8c995eaa98282f549e6f7f2
azarouski <azarouski@solvd.com>
2025-10-11 15:48:32 +0200

    `backup` function was refactored

--- patch/backup.sh
+++ patch/backup.sh
@@ -1,21 +1,63 @@
 #!/bin/bash
 
 backup() {
+  echo -e "\n==== Backup MCloud Agent in '$ZEBRUNNER_MCLOUD_AGENT_DIR' ====\n"
+
   confirm "" "      Do you want to do a backup now?" "n" || exit 0
 
-  # copy udev related files into ./backup folder
-  cp /usr/local/bin/zebrunner-farm backup/
-  cp /usr/local/bin/mcloud-devices.txt backup/
-  cp /etc/udev/rules.d/90_mcloud.rules backup/
-  cp roles/devices/vars/main.yml roles/devices/vars/main.yml.bak
-  cp roles/mac-devices/vars/main.yml roles/mac-devices/vars/main.yml.bak
+  os="$(uname)" 2>/dev/null
+  required=( \
+  "backup/defaults/main.yml" \
+  "backup/templates/zebrunner-farm" \
+  "backup/templates/mcloud-devices.txt" \
+  "backup/vars/main.yml" )
 
-  if [ -f backup/zebrunner-farm ] && [ -f backup/mcloud-devices.txt ] && [ -f backup/90_mcloud.rules ] && [ -f roles/devices/vars/main.yml.bak ] && [ -f roles/mac-devices/vars/main.yml.bak ]; then
-    echo "MCloud backup succeed."
+  echo "Backing up MCloud Agent related files:"
+
+  if [ ! -d "backup/defaults" ]; then
+    echo "Creating directory for defaults/main.yml backup"
+    mkdir -vp backup/defaults
+  fi
+
+  if [ ! -d "backup/vars" ]; then
+    echo "Creating directory for roles/.../vars/main.yml backup"
+    mkdir -vp backup/vars
+  fi
+
+  if [ ! -d "backup/templates" ]; then
+    echo "Creating directory for rules backup"
+    mkdir -vp backup/templates
+  fi
+
+  cp -av defaults/main.yml backup/defaults/
+
+  cp -av /usr/local/bin/zebrunner-farm backup/templates/
+  cp -av /usr/local/bin/mcloud-devices.txt backup/templates/
+
+  if [ "$os" == "Darwin" ]; then
+    required+=( "backup/templates/ZebrunnerDevicesListener.plist" "backup/templates/ZebrunnerUsbmuxd.plist" )
+    cp -av "$HOME/Library/LaunchAgents/ZebrunnerDevicesListener.plist" backup/templates/
+    cp -av "$HOME/Library/LaunchAgents/ZebrunnerUsbmuxd.plist" backup/templates/
+    cp -av roles/mac-devices/vars/main.yml backup/vars/
   else
-    echo_warning "MCloud backup failed!"
+    required+=( "backup/templates/90_mcloud.rules" )
+    cp -av /etc/udev/rules.d/90_mcloud.rules backup/templates/
+    cp -av roles/devices/vars/main.yml backup/vars/
+  fi
+
+  missing=()
+  for f in "${required[@]}"; do
+    [[ -f "$f" ]] || missing+=("$f")
+  done
+
+  if (( ${#missing[@]} == 0 )); then
+    echo "MCloud backup succeeded."
+  else
+    echo_warning "MCloud backup failed! Missing: ${missing[*]}"
     echo_telegram
   fi
+
+  echo -e "\n===================================================================\n"
 }
 
 restore() {
