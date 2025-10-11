e1a994c57a4627fc5bc5ce95172fc19145d11f18
azarouski <azarouski@solvd.com>
2025-10-11 17:12:13 +0200

    `backup` was refactored

--- patch/backup.sh
+++ patch/backup.sh
@@ -5,7 +5,7 @@ backup() {
 
   confirm "" "      Do you want to do a backup now?" "n" || exit 0
   echo ""
-  echo "Creating new backup directory:"
+  echo -en "Creating new backup directory:\n    "
   local backup_dir_name
   backup_dir_name="backup/bak_$(date +%d-%m-%Y_%H-%M)_$(random_string 5)"
   if mkdir -vp "$backup_dir_name" 2>/dev/null; then
@@ -16,19 +16,19 @@ backup() {
   fi
 
   if [ ! -d "$backup_dir_name/defaults" ]; then
-    echo "Creating directory for $backup_dir_name/main.yml backup"
+    echo -en "Creating directory for $backup_dir_name/main.yml backup:\n    "
     mkdir -vp "$backup_dir_name/defaults" 2>/dev/null || failed "mkdir for '$backup_dir_name/defaults' failed"
     echo ""
   fi
 
   if [ ! -d "$backup_dir_name/vars" ]; then
-    echo "Creating directory for roles/.../vars/main.yml backup"
+    echo -en "Creating directory for roles/.../vars/main.yml backup:\n    "
     mkdir -vp "$backup_dir_name/vars" 2>/dev/null || failed "mkdir for '$backup_dir_name/vars' failed"
     echo ""
   fi
 
   if [ ! -d "$backup_dir_name/templates" ]; then
-    echo "Creating directory for rules backup"
+    echo -en "Creating directory for rules backup:\n    "
     mkdir -vp "$backup_dir_name/templates" 2>/dev/null || failed "mkdir for '$backup_dir_name/defaults' failed"
     echo ""
   fi
@@ -40,17 +40,25 @@ backup() {
     "$backup_dir_name/templates/mcloud-devices.txt" \
     "$backup_dir_name/vars/main.yml"\
     )
+  echo -n "    "
   cp -av defaults/main.yml "$backup_dir_name/defaults/" 2>/dev/null || failed "cp for 'defaults/main.yml' failed"
+  echo -n "    "
   cp -av /usr/local/bin/zebrunner-farm "$backup_dir_name/templates/" 2>/dev/null || failed "cp for '/usr/local/bin/zebrunner-farm' failed"
+  echo -n "    "
   cp -av /usr/local/bin/mcloud-devices.txt "$backup_dir_name/templates/" 2>/dev/null || failed "cp for '/usr/local/bin/mcloud-devices.txt' failed"
   if [ "$(uname)" == "Darwin" ]; then
     required+=( "$backup_dir_name/templates/ZebrunnerDevicesListener.plist" "$backup_dir_name/templates/ZebrunnerUsbmuxd.plist" )
+    echo -n "    "
     cp -av "$HOME/Library/LaunchAgents/ZebrunnerDevicesListener.plist" "$backup_dir_name/templates/" 2>/dev/null || failed "cp for '$HOME/Library/LaunchAgents/ZebrunnerDevicesListener.plist' failed"
+    echo -n "    "
     cp -av "$HOME/Library/LaunchAgents/ZebrunnerUsbmuxd.plist" "$backup_dir_name/templates/" 2>/dev/null || failed "cp for '$HOME/Library/LaunchAgents/ZebrunnerUsbmuxd.plist' failed"
+    echo -n "    "
     cp -av roles/mac-devices/vars/main.yml "$backup_dir_name/vars/" 2>/dev/null || failed "cp for 'roles/mac-devices/vars/main.yml' failed"
   else
     required+=( "$backup_dir_name/templates/90_mcloud.rules" )
+    echo -n "    "
     cp -av /etc/udev/rules.d/90_mcloud.rules "$backup_dir_name/templates/" 2>/dev/null || failed "cp for '/etc/udev/rules.d/90_mcloud.rules' failed"
+    echo -n "    "
     cp -av roles/devices/vars/main.yml "$backup_dir_name/vars/" 2>/dev/null || failed "cp for 'roles/devices/vars/main.yml' failed"
   fi
 
@@ -62,9 +70,12 @@ backup() {
   echo ""
   echo "Status:"
   if (( ${#missing[@]} == 0 )); then
-    succeed "MCloud backup succeeded to the directory: $backup_dir_name"
+    echo "MCloud backup succeeded!"
+    echo -n "    "
+    succeed "Backup directory: $backup_dir_name"
   else
     echo "MCloud backup failed!"
+    echo -n "    "
     failed "Missing: ${missing[*]}"
   fi
 
