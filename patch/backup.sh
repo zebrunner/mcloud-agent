1c5e83cce63dc20f4649a48570bf0a317475e0b5
azarouski <azarouski@solvd.com>
2025-10-11 16:06:05 +0200

    errors output was improver for backup function

--- patch/backup.sh
+++ patch/backup.sh
@@ -16,41 +16,41 @@ backup() {
   echo ""
   if [ ! -d "backup/defaults" ]; then
     echo "Creating directory for defaults/main.yml backup"
-    mkdir -vp backup/defaults
+    mkdir -vp backup/defaults 2>/dev/null || failed "mkdir for 'backup/defaults' failed"
     echo ""
   fi
 
   if [ ! -d "backup/vars" ]; then
     echo "Creating directory for roles/.../vars/main.yml backup"
-    mkdir -vp backup/vars
+    mkdir -vp backup/vars 2>/dev/null || failed "mkdir for 'backup/vars' failed"
     echo ""
   fi
 
   if [ ! -d "backup/templates" ]; then
     echo "Creating directory for rules backup"
-    mkdir -vp backup/templates
+    mkdir -vp backup/templates 2>/dev/null || failed "mkdir for 'backup/defaults' failed"
     echo ""
   fi
 
-  cp -av defaults/main.yml backup/defaults/
+  cp -av defaults/main.yml backup/defaults/ 2>/dev/null || failed "cp for 'defaults/main.yml' failed"
   echo ""
-  cp -av /usr/local/bin/zebrunner-farm backup/templates/
+  cp -av /usr/local/bin/zebrunner-farm backup/templates/ 2>/dev/null || failed "cp for '/usr/local/bin/zebrunner-farm' failed"
   echo ""
-  cp -av /usr/local/bin/mcloud-devices.txt backup/templates/
+  cp -av /usr/local/bin/mcloud-devices.txt backup/templates/ 2>/dev/null || failed "cp for '/usr/local/bin/mcloud-devices.txt' failed"
   echo ""
   if [ "$os" == "Darwin" ]; then
     required+=( "backup/templates/ZebrunnerDevicesListener.plist" "backup/templates/ZebrunnerUsbmuxd.plist" )
-    cp -av "$HOME/Library/LaunchAgents/ZebrunnerDevicesListener.plist" backup/templates/
+    cp -av "$HOME/Library/LaunchAgents/ZebrunnerDevicesListener.plist" backup/templates/ 2>/dev/null || failed "cp for '$HOME/Library/LaunchAgents/ZebrunnerDevicesListener.plist' failed"
     echo ""
-    cp -av "$HOME/Library/LaunchAgents/ZebrunnerUsbmuxd.plist" backup/templates/
+    cp -av "$HOME/Library/LaunchAgents/ZebrunnerUsbmuxd.plist" backup/templates/ 2>/dev/null || failed "cp for '$HOME/Library/LaunchAgents/ZebrunnerUsbmuxd.plist' failed"
     echo ""
-    cp -av roles/mac-devices/vars/main.yml backup/vars/
+    cp -av roles/mac-devices/vars/main.yml backup/vars/ 2>/dev/null || failed "cp for 'roles/mac-devices/vars/main.yml' failed"
     echo ""
   else
     required+=( "backup/templates/90_mcloud.rules" )
-    cp -av /etc/udev/rules.d/90_mcloud.rules backup/templates/
+    cp -av /etc/udev/rules.d/90_mcloud.rules backup/templates/ 2>/dev/null || failed "cp for '/etc/udev/rules.d/90_mcloud.rules' failed"
     echo ""
-    cp -av roles/devices/vars/main.yml backup/vars/
+    cp -av roles/devices/vars/main.yml backup/vars/ 2>/dev/null || failed "cp for 'roles/devices/vars/main.yml' failed"
     echo ""
   fi
 
