7a141f21a32b4e9a2b752cd5dbf38867df9faf28
azarouski <azarouski@solvd.com>
2025-10-11 22:14:19 +0200

    `restore` function was refactored

--- patch/backup.sh
+++ patch/backup.sh
@@ -62,25 +62,109 @@ backup() {
 }
 
 restore() {
+  echo -e "\n==== Backup MCloud Agent in '$ZEBRUNNER_MCLOUD_AGENT_DIR' ====\n"
+
   confirm "" "      Your services will be stopped and current data might be lost. Do you want to do a restore now?" "n" || exit 0
 
-  sudo cp backup/zebrunner-farm /usr/local/bin/zebrunner-farm
-  sudo cp backup/mcloud-devices.txt /usr/local/bin/mcloud-devices.txt
-  sudo cp backup/90_mcloud.rules /etc/udev/rules.d/90_mcloud.rules
-  cp roles/devices/vars/main.yml.bak roles/devices/vars/main.yml
-  cp roles/mac-devices/vars/main.yml.bak roles/mac-devices/vars/main.yml
+  echo ""
+  ask_for_sudo || {
+    echo_warning "Sudo permissions are required to run this script!"
+    exit 1
+  }
+  echo ""
+  echo "Backups found in 'backup' directory:"
+  for bup in backup/bak_*; do
+    if [ -d "$bup" ]; then
+      echo "    $(basename "$bup")"
+    fi
+  done
 
-  # reload udevadm rules
-  sudo udevadm control --reload-rules
+  echo ""
+  while true; do
+    read -r -p "Please enter the backup directory name you want to restore: " backup_dir_name
+    if [ -d "backup/$backup_dir_name" ]; then
+      backup_dir_name="backup/$backup_dir_name"
+    else
+      echo "Directory 'backup/$backup_dir_name' does not exist. Please try again."
+    fi
+  done
 
-  if [ -f /usr/local/bin/zebrunner-farm ] && [ -f /usr/local/bin/mcloud-devices.txt ] && [ -f /etc/udev/rules.d/90_mcloud.rules ] && [ -f roles/devices/vars/main.yml ] && [ -f roles/mac-devices/vars/main.yml ]; then
-    echo "MCloud restore succeed."
+  echo "Backing up:"
+  echo -n "    "
+  cp -av "$backup_dir_name/defaults/main.yml" "defaults/main.yml" 2>/dev/null || failed "cp for 'defaults/main.yml' failed"
+  echo -n "    "
+  sudo cp -av "$backup_dir_name/templates/zebrunner-farm" "/usr/local/bin/zebrunner-farm" 2>/dev/null || failed "cp for '$backup_dir_name/templates/zebrunner-farm' failed"
+  echo -n "    "
+  sudo cp -av "$backup_dir_name/templates/mcloud-devices.txt" "/usr/local/bin/mcloud-devices.txt" 2>/dev/null || failed "cp for '$backup_dir_name/templates/mcloud-devices.txt' failed"
+  if [ "$(uname)" == "Darwin" ]; then
+    echo -n "    "
+    cp -av "$backup_dir_name/vars/main.yml" "roles/mac-devices/vars/main.yml" 2>/dev/null || failed "cp for 'roles/mac-devices/vars/main.yml' failed"
+
+    echo -n "    "
+    echo "Unloading ZebrunnerDevicesListener.plist, replacing, loading again:"
+    if launchctl list com.zebrunner.mcloud; then
+       if launchctl bootout gui/"$(id -u)" "$HOME/Library/LaunchAgents/ZebrunnerDevicesListener.plist" 2>/dev/null; then
+         echo -n "    "; echo -n "    "
+         echo "'$HOME/Library/LaunchAgents/ZebrunnerDevicesListener.plist' stopped"
+         echo -n "    "; echo -n "    "
+          if sudo cp -av "$backup_dir_name/templates/ZebrunnerDevicesListener.plist" "$HOME/Library/LaunchAgents/ZebrunnerDevicesListener.plist" 2>/dev/null; then
+            if launchctl bootstrap gui/"$(id -u)" "$HOME/Library/LaunchAgents/ZebrunnerDevicesListener.plist" 2>/dev/null && launchctl enable gui/"$(id -u)"/com.zebrunner.mcloud; then
+              echo -n "    "; echo -n "    "
+              echo "'$HOME/Library/LaunchAgents/ZebrunnerDevicesListener.plist' started"
+            else
+              echo -n "    "; echo -n "    "
+              failed "launchctl bootstrap for '$HOME/Library/LaunchAgents/ZebrunnerDevicesListener.plist' failed"
+            fi
+          else
+            echo -n "    "; echo -n "    "
+            failed "cp for '$HOME/Library/LaunchAgents/ZebrunnerDevicesListener.plist' failed"
+          fi
+       fi
+    else
+      echo -n "    "; echo -n "    "
+      echo "'$HOME/Library/LaunchAgents/ZebrunnerDevicesListener.plist' is not running"
+      echo -n "    "; echo -n "    "
+      sudo cp -av "$backup_dir_name/templates/ZebrunnerDevicesListener.plist" "$HOME/Library/LaunchAgents/ZebrunnerDevicesListener.plist" 2>/dev/null || failed "cp for '$HOME/Library/LaunchAgents/ZebrunnerDevicesListener.plist' failed"
+    fi
+
+    echo -n "    "
+    echo "Unloading ZebrunnerUsbmuxd.plist, replacing, loading again:"
+    if launchctl list com.zebrunner.usbmuxd; then
+       if launchctl bootout gui/"$(id -u)" "$HOME/Library/LaunchAgents/ZebrunnerUsbmuxd.plist" 2>/dev/null; then
+         echo -n "    "; echo -n "    "
+         echo "'$HOME/Library/LaunchAgents/ZebrunnerUsbmuxd.plist' stopped"
+         echo -n "    "; echo -n "    "
+          if sudo cp -av "$backup_dir_name/templates/ZebrunnerUsbmuxd.plist" "$HOME/Library/LaunchAgents/ZebrunnerUsbmuxd.plist" 2>/dev/null; then
+            if launchctl bootstrap gui/"$(id -u)" "$HOME/Library/LaunchAgents/ZebrunnerUsbmuxd.plist" 2>/dev/null && launchctl enable gui/"$(id -u)"/com.zebrunner.usbmuxd; then
+              echo -n "    "; echo -n "    "
+              echo "'$HOME/Library/LaunchAgents/ZebrunnerUsbmuxd.plist' started"
+            else
+              echo -n "    "; echo -n "    "
+              failed "launchctl bootstrap for '$HOME/Library/LaunchAgents/ZebrunnerUsbmuxd.plist' failed"
+            fi
+          else
+            echo -n "    "; echo -n "    "
+            failed "cp for '$HOME/Library/LaunchAgents/ZebrunnerUsbmuxd.plist' failed"
+          fi
+       fi
+    else
+      echo -n "    "; echo -n "    "
+      echo "'$HOME/Library/LaunchAgents/ZebrunnerUsbmuxd.plist' is not running"
+      echo -n "    "; echo -n "    "
+      sudo cp -av "$backup_dir_name/templates/ZebrunnerUsbmuxd.plist" "$HOME/Library/LaunchAgents/ZebrunnerUsbmuxd.plist" 2>/dev/null || failed "cp for '$HOME/Library/LaunchAgents/ZebrunnerUsbmuxd.plist' failed"
+    fi
+
   else
-    echo_warning "MCloud restore failed!"
-    echo_telegram
+    echo -n "    "
+    sudo cp -av "$backup_dir_name/templates/90_mcloud.rules" "/etc/udev/rules.d/90_mcloud.rules" 2>/dev/null || failed "cp for 'roles/devices/vars/main.yml' failed"
+    echo -n "    "
+    cp -av "$backup_dir_name/vars/main.yml" "roles/devices/vars/main.yml" 2>/dev/null || failed "cp for 'roles/devices/vars/main.yml' failed"
+
+    # reload udevadm rules
+    sudo udevadm control --reload-rules
   fi
 
-  down
+  echo_warning "Your services needs to be restarted after restore."
 
-  echo_warning "Your services needs to be started after restore."
+  echo -e "\n===================================================================\n"
 }
